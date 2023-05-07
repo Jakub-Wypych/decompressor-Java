496d28c26be4060d2f900ef371b0b76f0c134813
Jakub Wypych <01178898@pw.edu.pl>
2023-05-07 16:39:33 +0200

    writing and cutting #15
    Finished implementing Bitwrite
    Finished implementing Turncate
    Bitwrite can detect leftover bits
    Turncate can detect if program is trying to cut more than it should
    Added garbage.bin for testing purposes
    Main can read outfilepath now
    Made Decompress class close file operator classes at end
    Overall the decompression part has been fully finished,
    all that is left now is the graphical interface

--- decompressor-Java/src/main/java/decompressor/Bitwork/Bitwrite.java
+++ decompressor-Java/src/main/java/decompressor/Bitwork/Bitwrite.java
@@ -2,43 +2,70 @@ package decompressor.Bitwork;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /* Writes bits into file from given Arraylist of bits
  */
 public class Bitwrite {
     private final FileOutputStream outfile;
-    private ArrayList<Byte> buffer; // Byte to be written
+    private final ArrayList<Byte> buffer; // Byte to be written
 
     public Bitwrite(String filepath) {
-        if(filepath == null) { // write into default file
-            filepath = "decompressed.bin";
-        }
         try {
             outfile = new FileOutputStream(filepath);
         } catch (FileNotFoundException e) {
             System.out.println("ERROR: Couldn't open file: " + filepath);
             throw new RuntimeException(e);
         }
+        buffer = new ArrayList<>();
     }
     public void writebits(ArrayList<Byte> bits) {
+        for (Byte bit : bits) {
+            if (buffer.size() == 8)
+                writebyte();
+            buffer.add(bit);
+        }
+        if (buffer.size() == 8)
+            writebyte();
+    }
+
+    private void writebyte() {
         int char_value = 0;
-        if(bits.get(0) == 1)
+        if(buffer.get(0) == 1)
             char_value += 128;
-        if(bits.get(1) == 1)
+        if(buffer.get(1) == 1)
             char_value += 64;
-        if(bits.get(2) == 1)
+        if(buffer.get(2) == 1)
             char_value += 32;
-        if(bits.get(3) == 1)
+        if(buffer.get(3) == 1)
             char_value += 16;
-        if(bits.get(4) == 1)
+        if(buffer.get(4) == 1)
             char_value += 8;
-        if(bits.get(5) == 1)
+        if(buffer.get(5) == 1)
             char_value += 4;
-        if(bits.get(6) == 1)
+        if(buffer.get(6) == 1)
             char_value += 2;
-        if(bits.get(7) == 1)
+        if(buffer.get(7) == 1)
             char_value += 1;
-        System.out.print((char) char_value);
+        try {
+            outfile.write(char_value);
+        } catch (IOException e) {
+            System.out.println("ERROR: Couldn't write into outfile!");
+            throw new RuntimeException(e);
+        }
+        buffer.clear();
+    }
+
+    public int getBufferSize() {
+        return buffer.size();
+    }
+    public void close() {
+        try {
+            outfile.close();
+        } catch (IOException e) {
+            System.out.println("ERROR: Failure in closing the outfile!");
+            throw new RuntimeException(e);
+        }
     }
 }
