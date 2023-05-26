d8e2504a5c3ff72866e5595f87ce57f8bc20f800
Jakub Wypych <01178898@pw.edu.pl>
2023-05-26 19:11:17 +0200

    Tying up loose ends #23 part .3
    Added scrolling to tree view
    Added click and drag to tree view

--- decompressor-Java/src/main/java/graphical/decompressorjava/resultsView/ResultsViewController.java
+++ decompressor-Java/src/main/java/graphical/decompressorjava/resultsView/ResultsViewController.java
@@ -6,14 +6,20 @@ import graphical.decompressorjava.resultsView.treeDrawer.TreeDrawer;
 import graphical.decompressorjava.resultsView.treeDrawer.TreePosModifier;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 /* Shows results of decompression
  */
 public class ResultsViewController {
 
+    @FXML ScrollPane scrollPane;
     @FXML AnchorPane canvas;
     @FXML Label identLabel, dictionaryLabel;
+
+    private double previous_x;
+    private double previous_y;
 
     public void setAll(Results results) {
         this.identLabel.setText(results.ident().toString());
@@ -22,9 +28,21 @@ public class ResultsViewController {
             dictionaryLabeltxt.append(rd.toString()).append("\n");
         }
         this.dictionaryLabel.setText(dictionaryLabeltxt.toString());
-        TreePosModifier modifier = new TreePosModifier(0, 30, 10, 30, 30);
+        TreePosModifier modifier = new TreePosModifier(0, 40, 10, 40, 30);
         TreeDrawer treeDrawer = new TreeDrawer(results.tree().getRoot(), canvas, modifier);
         treeDrawer.setModifier(modifier);
         treeDrawer.drawTree();
+    }
+
+    public void onClickAndDrag(MouseEvent mouseEvent) {
+        scrollPane.setHvalue(scrollPane.getHvalue()-(mouseEvent.getX()-previous_x)/canvas.getWidth());
+        scrollPane.setVvalue(scrollPane.getVvalue()-(mouseEvent.getY()-previous_y)/canvas.getHeight());
+        previous_x = mouseEvent.getX();
+        previous_y = mouseEvent.getY();
+    }
+
+    public void onMousePressed(MouseEvent mouseEvent) {
+        previous_x = mouseEvent.getX();
+        previous_y = mouseEvent.getY();
     }
 }
